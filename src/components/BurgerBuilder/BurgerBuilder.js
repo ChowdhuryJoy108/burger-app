3cb510c33df80fb3d7a528f9c9c0318a8a63e317
Joy Chowdhury <imjoychy69@gmail.com>
2022-07-16 19:28:19 +0600

    initial state

---
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BurgerBuilder = () =>{
+    return(
+        <div>
+            <h1 class="text-warning bg-dark">hello Burger!</h1>
+           
+        </div>
+    )
+}
+export default BurgerBuilder;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+
+} from 'reactstrap';
+import "./Header.css"
+import Logo from '../../Assets/logo.png'
+
+
+const Header = () =>{
+    return(
+        <div className="Navigation">
+            <Navbar style={{
+                backgroundColor:'#D70F64',
+                height:'70px',
+
+            }}>
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+                    <img src={Logo} alt="logo" width="80px" />
+                </NavbarBrand>
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="#" className="NavLink">Something</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+}
+export default Header;
