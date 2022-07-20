d69e84623f627acdd71fc6f28795d4ce74436bcb
Joy Chowdhury <imjoychy69@gmail.com>
2022-07-20 12:08:13 +0600

    adding firebase google and pass our data to server

--- src/components/Orders/Checkout/Checkout.js
+++ src/components/Orders/Checkout/Checkout.js
@@ -1,6 +1,18 @@
 import React,{Component} from "react";
 import {Button} from 'reactstrap'
 
+import {connect} from 'react-redux';
+import axios from 'axios';
+
+
+const mapStateToProps = (state) => {
+    return {
+        ingredients:state.ingredients,
+        totalPrice:state.totalPrice,
+        purchasable:state.purchasable,
+    }
+}
+
 class Checkout extends Component{
     state = {
         values:{
@@ -24,12 +36,30 @@ class Checkout extends Component{
     }
 
     submitHandler = () =>{
-        console.log(this.state.values);
+       const order = {
+            ingredients:this.props.ingredients,
+            customer : this.state.values,
+            price : this.props.totalPrice,
+            orderTime:new Date(),
+
+       }
+       axios.post("https://burger-app-29c23-default-rtdb.firebaseio.com/orders.json",order)
+       .then(res => console.log(res))
+       .catch(err => console.log(err))
+       console.log(order);
     }
 
     render(){
        return(
          <div>
+            <h4 style={{
+                border:'1px solid gray',
+                padding:'20px',
+                boxShadow:'1px 1px #888888',
+                borderRadius:'5px',
+
+            }}>Payment:{this.props.totalPrice}BDT</h4>
+
             <form style={{
                 border:'1px solid gray',
                 padding:'20px',
@@ -55,4 +85,4 @@ class Checkout extends Component{
        )
     }
 }
-export default Checkout;
+export default connect(mapStateToProps)(Checkout);
